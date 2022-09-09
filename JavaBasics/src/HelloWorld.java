0064c3b9a3fc1dadc167b332ed268a3773f73134
Koushik Tutika <109944673+KoushikTutika@users.noreply.github.com>
2022-09-09 19:51:02 +0530

    Add files via upload

---
+++ JavaBasics/src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Fuck the world");
+	}
+
+	static {
+		System.out.println("Static Block 2");
+	}
+	static {
+		System.out.println("Static Block 1");
+	}
+
+}
